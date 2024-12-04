c575269a7f5bbfe5f2cddcfd490759c49e9efefd
Dongwon Kang <dongwon.dwk@gmail.com>
2024-12-04 14:52:09 -0800

    styled on footer, changed layout of website

--- footer.php
+++ footer.php
@@ -13,14 +13,15 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo get_home_url(); ?>"><img src="<?php echo esc_url( wp_get_attachment_url( get_theme_mod( 'custom_logo' ) ) ); ?>" alt="site logo" class="footer-site-logo"></a>
-			<p class="copyright">Copyright &copy; <?php echo date('Y'); ?> Whitespace Studio</p>
 			<div class="legal-stuff">
 				<ul>
 					<li><a href="<?php echo get_permalink( get_page_by_path('privacy-policy') ); ?>">Privacy Policy</a></li>
+					<p class="footer-separator">|</p>
 					<li><a href="<?php echo get_permalink( get_page_by_path('sample-page') ); ?>">Sample Page</a></li>
 				</ul>
 			</div>
+			<a href="<?php echo get_home_url(); ?>" class="footer-site-logo">Whitespace Studio</a>
+			<p class="copyright">Copyright &copy; <?php echo date('Y'); ?> Whitespace Studio</p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
@@ -29,3 +30,11 @@
 
 </body>
 </html>
+
+<?php
+/** 
+ * get logo
+ * <img src="<?php echo esc_url( wp_get_attachment_url( get_theme_mod( 'custom_logo' ) ) ); ?>" alt="site logo" class="footer-site-logo">
+ *
+ */
+?>
